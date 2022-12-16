13c5f6fbc699bf16e89add8d971c61807eb9acd5
JesusBHZ <1721110061@utectulancingo.edu.mx>
2022-12-16 16:43:49 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Handling-DOM-with-JS
 Practicas de escritorio del manejo del DOM con JavaScript
+
+<br>
+
+## DOM - DOCUMENT OBJECT MODEL
+- Representacion del HTML
+
+- Estructura de datos arbol
+
+- Nodos
+
+![Esta es una imagen de ejemplo](assets/DOM.png)
+
---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+// Encontrar etiquetas por el nombre de clase o etiqueta
+let elemento;
+elemento = document.querySelector('h2'); 
+console.log(elemento);*/
+
+/*
+// // Modifica el contenido de una etiquet por ID
+const elemento = document.getElementById('t-curso');
+console.log(elemento)
+
+elemento.innerHTML = "Clases del 2022" a
+
+const elemento2 = document.getElementById('mensajes');
+console.log(elemento2)*/
