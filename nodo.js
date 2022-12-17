96287567026e261b99dbfc793b5c36594750f9dd
JesusBHZ <1721110061@utectulancingo.edu.mx>
2022-12-17 15:43:51 -0600

    Nodo second part

--- nodo.js
+++ nodo.js
@@ -1,3 +1,4 @@
+/*
 // Obtener un elemento por su ID
 document.getElementById('id');
 
@@ -6,7 +7,7 @@ document.getElementsByTagName('name');
 
 // Para obtener un elemento por el nombre de una clase
 document.getElementsByClassName('class');
-
+*/
 
 // ¿por que no usar querySelector? 
 /* Con querySelector tenemos que ser especificos con el nodo a obtener,
@@ -17,6 +18,44 @@ example:
 elemento = document.querySelector('#titulo');  // el elemento con el ID 'titulo'
  document.querySelector('#titulo') : solo uno
  document.querySelectorAll('#titulo') : todos
+*/
+
+/*
+// Crear nodos 
+const padre = document.querySelector('section.contenedor'); // Nodo padre donde estara el nodo nuevo
+console.log(padre)
+
+// Creacion de un h2 con el texto: JS, sera el ultimo elemento del padre
+const nuevo_nodo = document.createElement('h2');
+padre.appendChild(nuevo_nodo)
+const texto = document.createTextNode('JS');
+nuevo_nodo.appendChild(texto);
 
 
+// Creacion de un p con el texto: modelos nuevos, sera el ultimo elemento del padre (debajo del h2)
+padre.append('modelos nuevos', document.createElement('p'));
+
+
+// creacion de un nuevo nodo h2 con el texto: Nodo, SE CREARA ANTES DE LA TABLA, NOSOTROS SELECCIONAMOS DONDE SEA CREA ANTES DE UN ELEMENTO
+const nuevo = document.createElement('h2');
+const limite = document.querySelector('table.tabla');
+console.log(limite);
+padre.insertBefore(nuevo,limite);
+const texto2 = document.createTextNode('Nodo');
+nuevo.appendChild(texto2);
+*/
+
+/*
+before: antes de
+after: después
+*/
+/*
+Creaion de nodos donde queramos 
+const ndo = document.createElement('h3');
+//limite.insertAdjacentElement("beforebegin",ndo);
+limite.insertAdjacentElement("afterbegin",ndo);
+//limite.insertAdjacentElement("beforeend",ndo);
+//limite.insertAdjacentElement("afterend",ndo);
+const texto3 = document.createTextNode('aqui');
+ndo.appendChild(texto3);
 */
