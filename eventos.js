9679774d71e9fb03f5cb01c8ebe15dd01f5ae2a5
JesusBHZ <1721110061@utectulancingo.edu.mx>
2022-12-20 16:04:48 -0600

    mouseevents

--- eventos.js
+++ eventos.js
@@ -1,4 +1,4 @@
-/* Eventos: Son la forma de controlar acciones en nuestra pagina web */
+/* Eventos: Son la forma de controlar acciones en nuestra pagina web 
 
 const enviar = document.getElementById('mensaje');
 
@@ -10,3 +10,35 @@ function mensaje1(){
 function mensaje2(){
     console.log('mensaje enviado');
 }
+*/
+
+/*------------- Parte 2 ------------- 
+nodo.addEventListener : Registra un evento a un nodo en especifico
+*/
+const enviar = document.getElementById('enviarr');
+const evento = () =>{
+    console.log('NUMERO DE CLIKS: ')
+}
+
+enviar.addEventListener('click',evento);
+
+const nombre = document.getElementById('txtTipos');
+
+const evento2 = () =>{
+    console.log('NUMERO DE LETRAS: ')
+}
+
+nombre.addEventListener('input',evento2);
+
+/* nodo.removeEventListener : Metodo que elimina un controlador de eventos */
+
+nombre.removeEventListener('input',evento2);
+
+enviar.removeEventListener('click',evento);
+
+const evento3 = function action(){
+    console.log('NUMERO DE ENVIOS')
+}
+
+enviar.addEventListener('click',evento3);
+
---
+++ mouseevents.js
@@ -0,0 +1,34 @@
+/* Eventos del mouse 
+
+ONCLICK: Evento que ocurre cuando el usuario hace click en un elemento 
+    <div id="caja" onclick="evento()"> Eventos del mouse</div>
+
+ONCONTEXTMENU: Evento que ocurre cuando el usuario hace click con el boton derecho en un elementi para abrir un menu contextual
+    <div id="caja" oncontextmenu="evento()"> Eventos del mouse</div>
+
+ONDBLCLICK: El evento ocurre cuando el usuario hace doble click en un elemento
+    <div id="caja" ondblclick="evento()"> Eventos del mouse</div>
+
+ONMOUSEDOWN: El evento ocurre cuando el usuario presiona un boton del mouse sobre un elemento
+    <div id="caja" onmousedown="evento()"> Eventos del mouse</div>
+    // presiona btn izq o der
+
+ONMOUSEENTER: El evento ocurre cuando el puntero se mueve sobre un elemento, como el HOVER de CSS
+    <div id="caja" onmouseenter="evento()"> Eventos del mouse</div>
+
+ONMOUSELEAVE: El evento ocurre cuando el puntero se mueve fuera de un elemento (del elemento hacia fuera)
+    <div id="caja" onmouseleave="evento()"> Eventos del mouse</div>
+
+ONMOUSEMOVE: El evento ocurre cuando el puntero se mueve mientras esta sobre un elemento
+    <div id="caja" onmousemove="evento()"> Eventos del mouse</div>
+
+ONMOUSEUP: El evento ocurre cuando el usuario suelta un boton del mouse sobre un elemento, se suelta el click
+    <div id="caja" onmouseup="evento()"> Eventos del mouse</div>
+
+
+*/
+
+
+function evento(){
+    alert('Mensaje enviado');
+}
